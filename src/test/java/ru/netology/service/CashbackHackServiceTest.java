461b7a5ed45d3b76c5bdd9fd37ffaee0d519a07d
Alissa-1 <alena.cheremukhina@gmail.com>
2022-08-10 21:50:00 +0300

    Jupiter & Vintage: 2 tests failed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,8 +3,11 @@ package ru.netology.service;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
+    // JUnit4
+
     @Test
     public void shouldCalcCashBackIfOverTheBound() {
         CashbackHackService service = new CashbackHackService();
@@ -31,4 +34,36 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         assertEquals("WRONG!", expected, actual);
     }
+
+
+    // JUPITER
+
+    @Test
+    public void shouldCalcCashBackIfOverTheBound0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1400;
+        int expected = 600;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual, "WRONG!");
+    }
+
+    @Test
+    public void shouldCalcCashBackIfUnderTheBound0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 300;
+        int expected = 700;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual, "WRONG!");
+    }
+
+    @Test
+    public void shouldCalcCashBackIfOnTheBound0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual, "WRONG!");
+    }
+
+
 }
